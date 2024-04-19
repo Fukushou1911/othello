4d93d1dac109b923a4135892d2178f6641d50f31
Fukushou1911 <s1f102401427@iniad.org>
2024-04-20 05:03:36 +0900

    講習1終わり

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,10 +17,10 @@ const Home = () => {
     console.log(x, y);
     const newBoard = structuredClone(board);
     newBoard[y][x] = turnColor;
-    if (turnColor === 1) {
-      setTurnColor(2);
-    } else {
-      setTurnColor(1);
+    setTurnColor(2 / turnColor);
+    if (board[y + 1] !== undefined && board[y + 1][x] === 2 / turnColor) {
+      newBoard[y][x] = turnColor;
+      setTurnColor(2 / turnColor);
     }
     setBoard(newBoard);
   };
